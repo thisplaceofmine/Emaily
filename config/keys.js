5004cb2754d41aa44317e13c81c675a1b3a867a8
thisplaceofmine <thisplaceofmine@users.noreply.github.com>
2019-08-14 18:18:32 +0900

    finihs auth flow

---
+++ config/keys.js
@@ -0,0 +1,12 @@
+if (process.env.NODE_ENV == 'production') {
+  module.exports = require('./prod')
+} else {
+  module.exports = require('./dev');
+}
+
+
+//c4dTjXbhjNiiVppp
+//mongodb+srv://myuser:c4dTjXbhjNiiVppp@cluster0-qu1au.azure.mongodb.net/test?retryWrites=true&w=majority
+
+//649007016891-l86merk6f1pqpvm5kktoutjr3uui00jf.apps.googleusercontent.com
+//LyexED-EyDoEegva5Eh-sT7y
---
+++ config/prod.js
@@ -0,0 +1,15 @@
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY
+};
+
+// module.exports = {
+//     googleClientID:
+//       '649007016891-l86merk6f1pqpvm5kktoutjr3uui00jf.apps.googleusercontent.com',
+//     googleClientSecret: 'LyexED-EyDoEegva5Eh-sT7y',
+//     mongoURI:
+//       'mongodb+srv://myuser:c4dTjXbhjNiiVppp@cluster0-qu1au.azure.mongodb.net/test?retryWrites=true&w=majority',
+//     cookieKey: 'thissplaceofmineisthiseplaceofminethatwasthiscplaceofmine'
+//   };
